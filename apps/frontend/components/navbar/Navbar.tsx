290da7cc68f2ca369cbba082d36b56827d709e24
kleinjeandaniel <kleinjeandaniel@gmail.com>
2025-02-20 23:07:41 +0100

    auto push

--- apps/frontend/components/navbar/Navbar.tsx
+++ apps/frontend/components/navbar/Navbar.tsx
@@ -69,10 +69,10 @@ export default function Navbar() {
           animate={{ opacity: 1, x: 0 }}
           exit={{ opacity: 0, x: 100 }}
         >
-          <button onClick={toggleMenu} className="absolute top-4 right-4 text-primary">
+          <button onClick={toggleMenu} className="absolute top-6 right-6 text-primary">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 text-primary"
+              className="h-8 w-8 text-primary"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -81,25 +81,24 @@ export default function Navbar() {
             </svg>
           </button>
           <div className="flex flex-col h-full w-full">
-            {/* Logo à gauche */}
-            <div className="flex items-center w-full justify-center">
-              <Link href="/" className="flex items-center gap-2">
-                <Image
-                  src="/logo/logo2.png"
-                  alt="Logo Pitaya"
-                  width={80}
-                  height={80}
-                  className="rounded-full drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] [filter:drop-shadow(0_4px_4px_rgba(0,0,0,0.25))]"
-                />
-                <div className="flex flex-col leading-none">
-                  <span className="text-3xl font-bold tracking-wide leading-none">Pitaya</span>
-                  <span className="text-xl font-light tracking-wider opacity-90 leading-none">micro crèche</span>
-                </div>
-              </Link>
-            </div>
-
             <div className="flex-col flex text-center items-center justify-center flex-grow gap-4">
-              <Link href="/equipe" className="text-xl" onClick={toggleMenu}>
+              {/* Logo au centre */}
+              <div className="flex items-center w-full justify-center">
+                <Link href="/" className="flex items-center gap-2" onClick={toggleMenu}>
+                  <Image
+                    src="/logo/logo2.png"
+                    alt="Logo Pitaya"
+                    width={80}
+                    height={80}
+                    className="rounded-full drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] [filter:drop-shadow(0_4px_4px_rgba(0,0,0,0.25))]"
+                  />
+                  <div className="flex flex-col leading-none">
+                    <span className="text-3xl font-bold tracking-wide leading-none">Pitaya</span>
+                    <span className="text-xl font-light tracking-wider opacity-90 leading-none">micro crèche</span>
+                  </div>
+                </Link>
+              </div>
+              <Link href="/equipe" className="text-xl">
                 L'équipe
               </Link>
 
